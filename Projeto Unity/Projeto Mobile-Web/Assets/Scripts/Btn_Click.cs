770b72e55225f16e96ef459b86d7fb8a7c872834
camilafacens <54379899+camilafacens@users.noreply.github.com>
2020-03-22 18:38:16 -0300

    Menu

---
+++ Projeto Unity/Projeto Mobile-Web/Assets/Scripts/Btn_Click.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Btn_Click : MonoBehaviour {
+
+    public Image fadeOut;
+    Button btn;
+
+    string Scene;
+    private void Start()
+    {
+        btn = GetComponent<Button>();
+    }
+    public void Click()
+    {
+        btn.interactable = false;
+        fadeOut.gameObject.SetActive(true);
+    }
+
+    public void LoadScene(string scene1)
+    {
+        Scene = scene1;
+        Invoke("Click",0f);
+        StartCoroutine(enumerator());
+    }
+
+    public IEnumerator enumerator()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(Scene);
+    }
+}
+
---
+++ Projeto Unity/Projeto Mobile-Web/Assets/Scripts/Fade.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    public GameObject go1;
+    public GameObject go2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            StartCoroutine(FadeOut());
+        }
+    }
+    IEnumerator FadeOut()
+    {
+        for(float x = 1; x > 0; x -= 0.2f)
+        {
+            Color c1 = go1.GetComponent<Renderer>().material.color;
+            c1.a = x;
+            go1.GetComponent<Renderer>().material.color = c1;
+
+            Color c2 = go2.GetComponent<Renderer>().material.color;
+            c2.a = x;
+            go2.GetComponent<Renderer>().material.color = c2;
+
+            yield return null;
+        }
+    }
+}
